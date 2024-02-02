eb289f66f2f6e886075b00f27227aea5413e0c92
MathieuChe <mathieu.chelim@gmail.com>
2024-02-02 16:28:43 +0100

    Initial Commit

---
+++ SwiftUICleanArchitectureLearning/SwiftUICleanArchitectureLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureLearningApp.swift
+//  SwiftUICleanArchitectureLearning
+//
+//  Created by Mathieu CHELIM on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
